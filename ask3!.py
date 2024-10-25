c1da9d32800adc70e23239114c8bd6bb3c483106
Myrto12 <myrtom200@gmail.com>
2024-10-25 16:44:43 +0300

    Add files via upload

---
+++ ask3!.py
@@ -0,0 +1,20 @@
+a =input("Δώσε το a: ")
+k =int(input("Δώσε το k: "))
+a = list(a)
+i = 0
+c = len(a)//k
+d = len(a)%k
+L=[]
+list(L)
+if d!=0:
+        c+=1
+while i<c:
+    b =''.join(a[i*k:((i+1)*k)])
+    L.insert(i,b)
+    i=i+1
+print(L)
+L = map(int,L)
+print(sum(L))
+
+
+
